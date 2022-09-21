471099c05964daa8238ccc3e214f6a8806e78211
Michal Budzowski <michal.budzowski@sonarsource.com>
2022-09-21 14:37:30 +0200

    Add project files.

---
+++ ConsoleApp49/ResourceHolder.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp49
+{
+    public class ResourceHolder : IDisposable
+    {
+        private System.IO.FileStream fs;
+        public void OpenResource(string path)
+        {
+            this.fs = new FileStream(path, FileMode.Open);
+        }
+        public void CloseResource()
+        {
+            this.fs.Close();
+        }
+
+        public void CleanUp()
+        {
+            this.fs.Dispose(); 
+        }
+
+        public void CleanUpWithCheck1()
+        {
+            this.fs?.Dispose();
+        }
+
+        public void CleanUpWithCheck2()
+        {
+            if (this.fs != null)
+            {
+                this.fs.Dispose();
+            }
+        }
+
+        public void Dispose()
+        {
+            this.fs.Dispose();
+        }
+    }
+}
