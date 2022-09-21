a2d4f20c4617a0f038f1ee1a361ebc930e9368b4
Michal Budzowski <michal.budzowski@sonarsource.com>
2022-09-21 14:51:32 +0200

    comments

--- ConsoleApp49/ResourceHolder.cs
+++ ConsoleApp49/ResourceHolder.cs
@@ -1,9 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+
 
 namespace ConsoleApp49
 {
@@ -21,19 +18,21 @@ namespace ConsoleApp49
 
         public void CleanUp()
         {
-            this.fs.Dispose(); 
+            this.fs.Dispose(); //S2952 triggered calling to
+                               //"Move this 'Dispose' call into this class' own 'Dispose' method.",
+                               //but 'Dispose' is already in class' own 'Dispose' method
         }
 
         public void CleanUpWithCheck1()
         {
-            this.fs?.Dispose();
+            this.fs?.Dispose(); // S2952 not triggered
         }
 
         public void CleanUpWithCheck2()
         {
             if (this.fs != null)
             {
-                this.fs.Dispose();
+                this.fs.Dispose(); //S2952 trggered although the checks seems equivalent to the one in CleanUpWithCheck1()
             }
         }
 
